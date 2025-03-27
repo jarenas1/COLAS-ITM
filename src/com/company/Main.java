6af7eace52a0ea4129ebdd7efcb19c26e1199aa1
Sala K 402 <salak402@salasfr.itm.edu.co>
2025-03-27 06:39:27 -0500

    add: methods and declaration of colas usind queue in main)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//DECLARACION DE COLAS
+        Queue<String> cola = new LinkedList<>();
+
+        //METODOS
+
+        /*
+        * add(Element) añade elemento al final de la cola
+        *
+        * offer(element) igual a add, pero si cola esta llena devuelve false
+        *
+        * remove() elimina el primer elemento de la cola
+        *
+        * poll() igual a remove, pero si cola esta vacio devuelve false
+        *
+        * element() devuelve elemento al inicio de la cola
+        *
+        * peek() igual que element pero si esta vacia la cola da null en vez de excepcion
+        *
+        * isEmpty() devuelve true o false
+        *
+        * size() devuelve el tamaño de elementos de la cola
+        * */
     }
 }
